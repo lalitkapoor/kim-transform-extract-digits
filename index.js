7e80f6af589478f6deef6445c9e134237acbe4d8
Lalit Kapoor <lalitkapoor@gmail.com>
2014-05-26 18:11:01 -0500

    support for default value

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-module.exports = function () {
+module.exports = function (def) {
   return function (value, object, array, callback) {
-    value = parseInt(value.replace(/[^0-9]/ig,''), 10) || 0
+    value = parseInt(value.replace(/[^0-9]/ig,''), 10)
+    if (isNaN(value)) value = def
     callback(null, value)
   }
 }
